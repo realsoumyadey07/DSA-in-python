adf43a0e64b0869e7fdf97e8a0c27dd97c3400a4
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-03-07 09:54:43 +0530

    singly linkedlist done

--- linkedList.py
+++ linkedList.py
@@ -19,9 +19,54 @@ class SLL:
             temp.next = n
         else:
             self.start = n
-
-            
-        
+    def search(self, data):
+            temp=self.start
+            while temp.next is not None:
+                if temp.item is data:
+                    return temp
+                temp=temp.next
+            return None
+    def insert_after(self, temp, data):
+        if temp is not None:
+            n=Node(data, temp.next)
+            temp.next = n
+    def print_list(self):
+        temp=self.start
+        while temp is not None:
+            print(temp.item,end=' ')
+            temp = temp.next
+    def delete_first(self):
+        if self.start is not None:
+            self.start= self.start.next
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            i = self.start
+            j = self.start.next
+            while j.next is not None:
+                i=i.next
+                j=j.next
+            i.next= None
+    def delete_item(self, data):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            if self.start.item is data:
+                self.start = None
+        else:
+            temp = self.start
+            while temp.next is not None:
+                if temp.item is data:
+                    temp = temp.next
+                temp = temp.next  
 
 
 myList = SLL()
+myList.insert_at_start(20)
+myList.insert_at_last(34)
+myList.insert_at_last(56)
+myList.insert_at_last(78)
+myList.print_list()
