3c757a2778a30e419761bd537f494dd7c75d21c3
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-03-18 10:03:07 +0530

    priority queue by linkedlist

--- priorityQueueList.py
+++ priorityQueueList.py
@@ -1,19 +1,19 @@
 class PriorityQueue:
     def __init__(self):
-        self.itemt = []
+        self.items = []
     def push(self, data, priority):
         index = 0
-        while index<len(self.itemt) and self.itemt[index][1]<priority:
+        while index<len(self.items) and self.items[index][1]<priority:
             index+=1
-        self.itemt.insert(index,(data, priority))
+        self.items.insert(index,(data, priority))
     def is_empty(self):
-        return len(self.itemt) is 0
+        return len(self.items) is 0
     def pop(self):
         if self.is_empty():
             raise IndexError('Priority Queue is empty')
-        return self.itemt.pop(0)[0]
+        return self.items.pop(0)[0]
     def size(self):
-        return len(self.itemt)
+        return len(self.items)
     
 
 
