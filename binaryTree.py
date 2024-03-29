114e93dcd5ff9247b3beeab72dfc208a6d1b31c9
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-03-29 10:07:01 +0530

    binary tree

--- binaryTree.py
+++ binaryTree.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self,left=None, item=None, right=None):
-        self.left = left
         self.item = item
+        self.left = left
         self.right = right
 class BST:
     def __init__(self):
@@ -25,7 +25,33 @@ class BST:
             return self.rsearch(root.left,data)
         else:
             return self.rsearch(root.right, data)
-
-
+    def inorder(self):
+        result = []
+        self.rinorder(self.root, result)
+        return result
+    def rinorder(self, root, result):
+        if root:
+            self.rinorder(root.left, result)
+            result.append(root.item)
+            self.rinorder(root.right, result)
+    def preorder(self):
+        result = []
+        self.rpreorder(self.root, result)
+        return result
+    def rpreorder(self, root, result):
+        if root:
+            result.append(root.item)
+            self.rpreorder(root.left, result)
+            self.rpreorder(root.right, result)
+    def postorder(self):
+        result = []
+        self.rpostorder(self.root, result)
+        return result
+    def rpostorder(self, root, result):
+        if root:
+            self.rpostorder(root.left, result)
+            root.append(root.item)
+            self.rpostorder(root.right, result)
+    
         
 
